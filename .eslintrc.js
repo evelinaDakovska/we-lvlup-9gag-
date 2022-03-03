8974ccd6602cb1859ff72d333868fbb5b7942a4c
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-03 15:06:51 +0200

    feat: airbnb implemented

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ["airbnb", "airbnb/hooks"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {
-    "linebreak-style": 0,
-  },
+  rules: {},
 };
