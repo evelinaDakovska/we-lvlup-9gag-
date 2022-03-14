b6d06e89c4e9ae4c3b23f16cd69ea80a2c72d056
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-14 14:03:28 +0200

    feat: home page improvements

--- src/components/homePage/home.js
+++ src/components/homePage/home.js
@@ -1,1 +1,18 @@
-console.log('iit works');
+let allMemes = [];
+
+let memeGenerator = await fetch("https://meme-api.herokuapp.com/gimme/50");
+const data = await memeGenerator.json();
+allMemes = data.memes;
+
+let allMemesDiv = document.getElementById("allMemes");
+allMemes.map((x) => {
+  let memeTitle = document.createElement("h3");
+  memeTitle.innerText = x.title;
+  let memeImg = document.createElement("img");
+  memeImg.src = x.preview[3];
+  let singleMeme = document.createElement("div");
+  singleMeme.className = "meme";
+  singleMeme.appendChild(memeTitle);
+  singleMeme.appendChild(memeImg);
+  allMemesDiv.appendChild(singleMeme);
+});
