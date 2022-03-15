84065f293d9e22c487d4808d1aa38daf55170c8b
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-15 10:21:37 +0200

    fix: styles and content fixed

--- src/components/registerPage/register.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("signUpEmail").addEventListener("click", classChange);
-function classChange() {
-  let appElement = document.getElementById("logApplications");
-  appElement.classList.add("hidden");
-  let emailElement = document.getElementById("regWithEmail");
-  emailElement.classList.remove("hidden");
-}
