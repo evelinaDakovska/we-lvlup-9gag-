6cfd35ec7b4ae6b77448967f7d3a14d8206dded0
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-15 16:31:53 +0200

    fix: router fixed

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,7 +2,7 @@ const route = (event) => {
   event = event || window.event;
   event.preventDefault();
   if (
-    `http://127.0.0.1:5500${window.location.pathname}` === event.target.href
+    `http://127.0.0.1:5503${window.location.pathname}` === event.target.href
   ) {
     return;
   }
@@ -21,16 +21,6 @@ const routes = {
     script: "./src/components/HomePage/home.js",
     style: "./src/components/HomePage/home.css",
   },
-/*   "/login": {
-    path: "./src/components/LoginPage/login.html",
-    script: "./src/components/LoginPage/login.js",
-    style: "./src/components/LoginPage/login.css",
-  },
-  "/register": {
-    path: "./src/components/RegisterPage/register.html",
-    script: "./src/components/RegisterPage/register.js",
-    style: "./src/components/RegisterPage/register.css",
-  }, */
 };
 
 const handleLocation = async () => {
