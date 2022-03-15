d36a974254c50709edef26bf4050de6212a14843
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-15 14:02:20 +0200

    feat: login with email implemented

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+
+localStorage.setItem("user", "");
+export let auth = getAuth();
+
+import { signOutFunc } from "./utils/user/signOut.js";
+
+export function onSignIn() {
+  document.getElementById("loginButton").remove();
+  document.getElementById("regButton").remove();
+
+  let navBarPart = document.getElementById("login-signup");
+
+  let profileButn = document.createElement("a");
+  profileButn.setAttribute("id", "profileButton");
+  profileButn.setAttribute("href", "/profile");
+  profileButn.setAttribute("onclick", "route()");
+  profileButn.innerText = "Profile";
+  navBarPart.appendChild(profileButn);
+
+  let signOut = document.createElement("a");
+  signOut.setAttribute("id", "signOutButton");
+  signOut.innerText = "Sign Out";
+  signOut.addEventListener("click", signOutFunc);
+  navBarPart.appendChild(signOut);
+}
--- src/utils/user/login.js
+++ src/utils/user/login.js
@@ -1,9 +1,31 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+
+import { auth, onSignIn } from "../../index.js";
 
 import facebookLogin from "./loginFacebook.js";
 import googleLogin from "./loginGoogle.js";
 
-const auth = getAuth();
-
 $("#signInFacebook").click(facebookLogin);
 $("#signInGoogle").click(googleLogin);
+
+const emailField = document.getElementById("logEmail");
+const passwordField = document.getElementById("logPassword");
+const signInWithMail = document.getElementById("signinButton");
+
+function signInWithEmailFunction() {
+  const email = emailField.value;
+  const password = passwordField.value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log("You're successfully signed in !");
+      localStorage.setItem("user", userCredential.user);
+      onSignIn();
+      $("#loginModal").modal("hide");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+signInWithMail.addEventListener("click", signInWithEmailFunction);
