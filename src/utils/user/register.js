157226efda981b421f255df7af689e5c8e3e84dc
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-15 14:02:41 +0200

    feat: register with email implemented

--- src/utils/user/register.js
+++ src/utils/user/register.js
@@ -1,46 +1,41 @@
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+
+import { auth, onSignIn } from "../../index.js";
 
 document.getElementById("signUpEmail").addEventListener("click", classChange);
 function classChange() {
-  let appElement = document.getElementById("logApplications");
-  appElement.classList.add("hidden");
   let emailElement = document.getElementById("regWithEmail");
   emailElement.classList.remove("hidden");
+  let appElement = document.getElementById("logApplications");
+  appElement.classList.add("hidden");
   signUpWithEmail();
 }
 
 function signUpWithEmail() {
   const fullName = document.getElementById("fullName");
   const emailField = document.getElementById("email");
-  const passwordField = document.getElementById("password");
+  const passwordField = document.getElementById("regPassword");
   const greeting = document.getElementById("greeting");
-  const auth = getAuth();
 
   const signUpFunction = () => {
     const email = emailField.value;
     const fName = fullName.value;
     const password = passwordField.value;
 
-    auth
-      .createUserWithEmailAndPassword(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log("Signed Up Successfully !");
-        user = userCredential.user;
-        document.getElementsByClassName("login-signup").classList.add("hidden");
-        document
-          .getElementsByClassName("greet-user")
-          .classList.remove("hidden");
+        localStorage.setItem("user", userCredential.user);
+        document.getElementById("regWithEmail").classList.add("hidden");
+        greeting.classList.remove("hidden");
+        onSignIn();
+        setTimeout(function () {
+          $("#registerModal").modal("hide");
+        }, 2000);
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
       });
-    greeting.classList.remove("hidden");
-    document.getElementById("regWithEmail").classList.add("hidden");
   };
-  document
-    .getElementById("regWithEmail")
-    .addEventListener("submit", signUpFunction);
+  document.getElementById("signUp").addEventListener("click", signUpFunction);
 }
