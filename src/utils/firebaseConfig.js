212865c74aedaa2f94435869857f74b5ffacc5b0
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-16 17:27:15 +0200

    feat: storage configurations

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_Wv28yBKGVSO3hVXzNtvUs6nJ3plw0Gw",
@@ -8,7 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "317040464210",
   appId: "1:317040464210:web:9edeabffcf78e5942e2a8b",
   measurementId: "G-NQJK277K5J",
+  storageBucket: "gs://gag-clone-78890.appspot.com/",
 };
 
 const app = initializeApp(firebaseConfig);
-
+const storage = getStorage(app);
