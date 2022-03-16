0a9db7a4786e72deea618d0a234061686859412b
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-16 12:18:13 +0200

    feat: facebook login

--- src/utils/user/loginFacebook.js
+++ src/utils/user/loginFacebook.js
@@ -3,29 +3,20 @@ import {
   FacebookAuthProvider,
 } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 
+import { auth, onSignIn } from "../../index.js";
+
 function facebookLogin() {
   const provider = new FacebookAuthProvider();
 
   signInWithPopup(auth, provider)
     .then((result) => {
-      const user = result.user;
-
+      localStorage.setItem("user", result.user);
       const credential = FacebookAuthProvider.credentialFromResult(result);
       const accessToken = credential.accessToken;
-
-      $(location).prop("href", "http://127.0.0.1:5500");
-      // ...
+      onSignIn();
     })
     .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = FacebookAuthProvider.credentialFromError(error);
-
-      // ...
+      console.log(error);
     });
 }
 
