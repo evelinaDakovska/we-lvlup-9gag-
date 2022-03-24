182e6b34aae0163d93edd7d08f8e805fc871044c
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-24 08:49:31 +0200

    feat: created reusable function for posts

--- src/components/memeDetails/memeDetails.js
+++ src/components/memeDetails/memeDetails.js
@@ -9,73 +9,14 @@ import {
 import { db, storage } from "../../utils/firebaseConfig.js";
 import { likeUnlikeFunc } from "../../utils/managePostData/likeUnlikeFunc.js";
 import { avatar } from "../../index.js";
+import { showSingleMeme } from "../../utils/managePostData/showPosts.js";
 
 window.memeDetails = async (memeID) => {
-  let currentMemeId = memeID.id;
+  const currentMemeId = memeID.id;
   const docRef = doc(db, "home", currentMemeId);
   const docSnap = await getDoc(docRef);
-  const currentMemeData = docSnap.data();
 
-  const currentUser = JSON.parse(localStorage.getItem("user"));
-  const currentUserId = currentUser.uid;
-
-  $("#titleMeme").text(currentMemeData.title);
-
-  const memeImg = document.createElement("img");
-  getDownloadURL(ref(storage, currentMemeData.url))
-    .then((url) => {
-      memeImg.setAttribute("src", url);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  $("#memeImgContainer").html(memeImg);
-
-  const likeBtn = document.createElement("div");
-  likeBtn.className = "upVoteBtn sectionBtns";
-  likeBtn.innerHTML = `<i class="fa-solid fa-arrow-up fa-lg"></i>`;
-  $("#currentMemeLike").html(likeBtn);
-
-  const likeCount = document.createElement("div");
-  likeCount.innerHTML = currentMemeData.likesCount;
-  likeBtn.appendChild(likeCount);
-
-  const unLikeBtn = document.createElement("div");
-  unLikeBtn.className = "downVoteBtn sectionBtns";
-  unLikeBtn.innerHTML = `<i class="fa-solid fa-arrow-down fa-lg"></i>`;
-  $("#currentMemeUnlike").html(unLikeBtn);
-
-  const unLikeCount = document.createElement("div");
-  unLikeCount.innerHTML = currentMemeData.unlikesCount;
-  unLikeBtn.appendChild(unLikeCount);
-
-  likeBtn.addEventListener("click", () =>
-    likeUnlikeFunc(
-      currentMemeId,
-      "like",
-      likeBtn,
-      unLikeBtn,
-      likeCount,
-      unLikeCount
-    )
-  );
-  unLikeBtn.addEventListener("click", () =>
-    likeUnlikeFunc(
-      currentMemeId,
-      "unlike",
-      likeBtn,
-      unLikeBtn,
-      likeCount,
-      unLikeCount
-    )
-  );
-
-  if (currentMemeData.likes.includes(currentUserId)) {
-    likeBtn.classList.add("activeLikeBtn");
-  }
-  if (currentMemeData.unlikes.includes(currentUserId)) {
-    unLikeBtn.classList.add("activeLikeBtn");
-  }
+  showSingleMeme(docSnap, "memeDetailsContainer");
 
   document.getElementById("userAvatar").src = avatar;
 };
--- src/utils/managePostData/showPosts.js
+++ src/utils/managePostData/showPosts.js
@@ -15,92 +15,109 @@ import { router } from "../navigoRouter.js";
 export async function showPosts(orderedDB, divId) {
   const querySnapshot = await getDocs(orderedDB);
   querySnapshot.forEach((doc) => {
-    const currentMeme = doc.data();
-    const memeTitle = currentMeme.title;
-    const memeURL = currentMeme.url;
-    const memeLikes = currentMeme.likes;
-    const memeUnlikes = currentMeme.unlikes;
-    const likesCount = currentMeme.likesCount;
-    const unlikesCount = currentMeme.unlikesCount;
-    const memeID = doc.id;
-
-    const currentUser = JSON.parse(localStorage.getItem("user"));
-    const currentUserId = currentUser.uid;
-
-    const allMemesDiv = document.getElementById(divId);
-
-    const memeImg = document.createElement("img");
-
-    getDownloadURL(ref(storage, memeURL))
-      .then((url) => {
-        memeImg.setAttribute("src", url);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    const imgContainer = document.createElement("div");
-    imgContainer.className = "imgContainer";
-    imgContainer.appendChild(memeImg);
-
-    const memeTitleElement = document.createElement("a");
-    memeTitleElement.className = "memeTitle";
-    memeTitleElement.innerText = memeTitle;
-    memeTitleElement.setAttribute("href", `/meme/${memeID}`);
-    memeTitleElement.setAttribute("data-navigo", "");
-    const singleMeme = document.createElement("div");
-    singleMeme.className = "meme";
-    const underMeme = document.createElement("div");
-    underMeme.className = "underMemeSection";
-
-    const likeBtn = document.createElement("div");
-    likeBtn.className = "upVoteBtn sectionBtns";
-    likeBtn.innerHTML = `<i class="fa-solid fa-arrow-up fa-lg"></i>`;
-    underMeme.appendChild(likeBtn);
-
-    const unLikeBtn = document.createElement("div");
-    unLikeBtn.className = "downVoteBtn sectionBtns";
-    unLikeBtn.innerHTML = `<i class="fa-solid fa-arrow-down fa-lg"></i>`;
-    underMeme.appendChild(unLikeBtn);
-
-    const likeCount = document.createElement("div");
-    likeCount.innerHTML = likesCount;
-    likeBtn.appendChild(likeCount);
-
-    const unLikeCount = document.createElement("div");
-    unLikeCount.innerHTML = unlikesCount;
-    unLikeBtn.appendChild(unLikeCount);
-
-    likeBtn.addEventListener("click", () =>
-      likeUnlikeFunc(memeID, "like", likeBtn, unLikeBtn, likeCount, unLikeCount)
-    );
-    unLikeBtn.addEventListener("click", () =>
-      likeUnlikeFunc(
-        memeID,
-        "unlike",
-        likeBtn,
-        unLikeBtn,
-        likeCount,
-        unLikeCount
-      )
-    );
-
-    if (memeLikes.includes(currentUserId)) {
-      likeBtn.classList.add("activeLikeBtn");
-    }
-    if (memeUnlikes.includes(currentUserId)) {
-      unLikeBtn.classList.add("activeLikeBtn");
-    }
-
-    const commentBtn = document.createElement("div");
-    commentBtn.className = "commentsBtn sectionBtns";
-    commentBtn.innerHTML = '<i class="fa-solid fa-message fa-lg"></i>';
-    commentBtn.addEventListener("click", () => commentFunc(memeID));
-    underMeme.appendChild(commentBtn);
-
-    singleMeme.appendChild(memeTitleElement);
-    singleMeme.appendChild(imgContainer);
-    singleMeme.appendChild(underMeme);
-    allMemesDiv.appendChild(singleMeme);
+    showSingleMeme(doc, divId);
   });
   router.updatePageLinks();
+}
+
+export function showSingleMeme(meme, divId) {
+  const currentMeme = meme.data();
+  const memeTitle = currentMeme.title;
+  const memeURL = currentMeme.url;
+  const memeLikes = currentMeme.likes;
+  const memeUnlikes = currentMeme.unlikes;
+  const likesCount = currentMeme.likesCount;
+  const unlikesCount = currentMeme.unlikesCount;
+  const memeID = meme.id;
+
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+  const currentUserId = currentUser.uid;
+
+  const allMemesDiv = document.getElementById(divId);
+
+  const memeImg = document.createElement("img");
+
+  getDownloadURL(ref(storage, memeURL))
+    .then((url) => {
+      memeImg.setAttribute("src", url);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  const imgContainer = document.createElement("div");
+  imgContainer.className = "imgContainer";
+  imgContainer.appendChild(memeImg);
+
+  const memeTitleElement = document.createElement("a");
+  memeTitleElement.className = "memeTitle";
+  memeTitleElement.innerText = memeTitle;
+  memeTitleElement.setAttribute("href", `/meme/${memeID}`);
+  memeTitleElement.setAttribute("data-navigo", "");
+  const singleMeme = document.createElement("div");
+  singleMeme.className = "meme";
+  const underMeme = document.createElement("div");
+  underMeme.className = "underMemeSection";
+
+  const likeBtn = document.createElement("div");
+  likeBtn.className = "upVoteBtn sectionBtns";
+  likeBtn.innerHTML = `<i class="fa-solid fa-arrow-up fa-lg"></i>`;
+  underMeme.appendChild(likeBtn);
+
+  const unLikeBtn = document.createElement("div");
+  unLikeBtn.className = "downVoteBtn sectionBtns";
+  unLikeBtn.innerHTML = `<i class="fa-solid fa-arrow-down fa-lg"></i>`;
+  underMeme.appendChild(unLikeBtn);
+
+  const likeCount = document.createElement("div");
+  likeCount.innerHTML = likesCount;
+  likeBtn.appendChild(likeCount);
+
+  const unLikeCount = document.createElement("div");
+  unLikeCount.innerHTML = unlikesCount;
+  unLikeBtn.appendChild(unLikeCount);
+
+  likeBtn.addEventListener("click", () =>
+    likeUnlikeFunc(
+      memeID,
+      "like",
+      likeBtn,
+      unLikeBtn,
+      likeCount,
+      unLikeCount,
+      cantLikeMessage
+    )
+  );
+  unLikeBtn.addEventListener("click", () =>
+    likeUnlikeFunc(
+      memeID,
+      "unlike",
+      likeBtn,
+      unLikeBtn,
+      likeCount,
+      unLikeCount,
+      cantLikeMessage
+    )
+  );
+
+  if (memeLikes.includes(currentUserId)) {
+    likeBtn.classList.add("activeLikeBtn");
+  }
+  if (memeUnlikes.includes(currentUserId)) {
+    unLikeBtn.classList.add("activeLikeBtn");
+  }
+
+  const commentBtn = document.createElement("div");
+  commentBtn.className = "commentsBtn sectionBtns";
+  commentBtn.innerHTML = '<i class="fa-solid fa-message fa-lg"></i>';
+
+  underMeme.appendChild(commentBtn);
+
+  const cantLikeMessage = document.createElement("div");
+  cantLikeMessage.className = "cantLikeMessage";
+  underMeme.appendChild(cantLikeMessage);
+
+  singleMeme.appendChild(memeTitleElement);
+  singleMeme.appendChild(imgContainer);
+  singleMeme.appendChild(underMeme);
+  allMemesDiv.appendChild(singleMeme);
 }
