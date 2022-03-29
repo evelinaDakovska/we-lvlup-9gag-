b56d3abad14ab61a0099d0503db1da5c870a6836
Evelina Dakovska <84570949+evelinaDakovska@users.noreply.github.com>
2022-03-29 17:50:36 +0300

    feat: profile page likes and coments info

--- src/components/ProfilePage/profile.js
+++ src/components/ProfilePage/profile.js
@@ -4,6 +4,8 @@ import {
   collection,
   query,
   where,
+  getDoc,
+  doc,
 } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
 import { db } from "../../utils/firebaseConfig.js";
 import { showPosts } from "../../utils/managePostData/showPosts.js";
@@ -11,6 +13,18 @@ import { showPosts } from "../../utils/managePostData/showPosts.js";
 window.profile = async () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const memesRef = collection(db, "home");
+  const userRef = doc(db, "users", user.uid);
+  const userSnap = await getDoc(userRef);
+  const userData = userSnap.data();
+
+  document.getElementById("userNameTitle").innerText = userData.name;
+  document.getElementById(
+    "totalLikes"
+  ).innerText = `Total likes: ${userData.like}`;
+  document.getElementById(
+    "totalComments"
+  ).innerText = `Total comments: ${userData.comments}`;
+
   const docRef = query(memesRef, where("userID", "==", user.uid));
   showPosts(docRef, "profilePage");
 };
